8e6fbbdb83c60fd81f84b3442793b05f363a43db
Jan Schnasse <schnasse@gmx.de>
2014-10-31 15:32:02 +0100

    add default constructor to Pair - for json binding

--- app/models/Pair.java
+++ app/models/Pair.java
@@ -12,6 +12,11 @@ public class Pair<L, R> {
     private final L left;
     private final R right;
 
+    public Pair() {
+	this.left = null;
+	this.right = null;
+    }
+
     public Pair(L left, R right) {
 	this.left = left;
 	this.right = right;
