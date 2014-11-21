a49fd78a72fd2e9814e95815e06be25323a97bbb
Jan Schnasse <schnasse@gmx.de>
2014-11-21 15:58:09 +0100

    fix test the redirection does not exist anymore

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -15,15 +15,12 @@
  *
  */
 import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
 import org.junit.Test;
 
-import play.libs.F.Callback;
 import play.libs.WS;
-import play.test.TestBrowser;
 
 /**
  * @author Jan Schnasse, schnasse@hbz-nrw.de
@@ -42,17 +39,6 @@ public class IntegrationTest {
 		assertThat(
 			WS.url("http://localhost:3333").get().get().getStatus())
 			.isEqualTo(200);
-	    }
-	});
-    }
-
-    @Test
-    public void serverWelcome() {
-	running(testServer(3333), HTMLUNIT, new Callback<TestBrowser>() {
-	    public void invoke(TestBrowser browser) {
-		browser.goTo("http://localhost:3333");
-		assertThat(browser.url()).isEqualTo(
-			"http://localhost:3333/public/docs/index.html");
 	    }
 	});
     }
