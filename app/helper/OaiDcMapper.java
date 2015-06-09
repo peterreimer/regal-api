e07d2d7032d7635c5083252a6fb2e0d74a4fde59
Jan Schnasse <jan@schnasse.eu>
2015-06-09 16:04:33 +0200

    Enhance OAI_DC Mapping

--- app/helper/OaiDcMapper.java
+++ app/helper/OaiDcMapper.java
@@ -23,6 +23,68 @@ public class OaiDcMapper {
 
     /**
      * @return DublinCore object of node
+     **/
+    /*-doc-type:preprint
+
+    doc-type:workingPaper
+
+    doc-type:article
+
+    doc-type:contributionToPeriodical
+
+    doc-type:PeriodicalPart
+
+    doc-type:Periodical
+
+    doc-type:book
+
+    doc-type:bookPart
+
+    doc-type:Manuscript
+
+    doc-type:StudyThesis
+
+    doc-type:bachelorThesis
+
+    doc-type:masterThesis
+
+    doc-type:doctoralThesis
+
+    doc-type:conferenceObject
+
+    doc-type:lecture
+
+    doc-type:review
+
+    doc-type:annotation
+
+    doc-type:patent
+
+    doc-type:report
+
+    doc-type:MusicalNotation
+
+    doc-type:Sound
+
+    doc-type:Image
+
+    doc-type:MovingImage
+
+    doc-type:StillImage
+
+    doc-type:CourseMaterial
+
+    doc-type:Website
+
+    doc-type:Software
+
+    doc-type:CarthographicMaterial
+
+    doc-type:ResearchData
+
+    doc-type:Other
+
+    doc-type:Text
      */
     public DublinCoreData getData() {
 
@@ -35,16 +97,22 @@ public class OaiDcMapper {
 		data.addTitle(l.getObject());
 	    } else if ("http://purl.org/dc/terms/creator".equals(l
 		    .getPredicate())) {
-		data.addCreator(l.getObject());
+		data.addCreator(l.getObjectLabel());
 	    } else if ("http://purl.org/dc/terms/issued".equals(l
 		    .getPredicate())) {
 		data.addDate(l.getObject());
 	    } else if ("http://purl.org/dc/elements/1.1/contributor".equals(l
 		    .getPredicate())) {
-		data.addContributor(l.getObject());
+		data.addContributor(l.getObjectLabel());
 	    } else if ("http://purl.org/dc/elements/1.1/creator".equals(l
 		    .getPredicate())) {
-		data.addCreator(l.getObject());
+		data.addCreator(l.getObjectLabel());
+	    } else if ("http://purl.org/dc/terms/contributor".equals(l
+		    .getPredicate())) {
+		data.addContributor(l.getObjectLabel());
+	    } else if ("http://purl.org/dc/terms/creator".equals(l
+		    .getPredicate())) {
+		data.addCreator(l.getObjectLabel());
 	    } else if ("http://purl.org/dc/elements/1.1/publisher".equals(l
 		    .getPredicate())) {
 		data.addPublisher(l.getObject());
@@ -58,10 +126,20 @@ public class OaiDcMapper {
 		data.addIdentifier(l.getObject(), "hbz:isbn");
 	    } else if ("http://purl.org/dc/elements/1.1/subject".equals(l
 		    .getPredicate())) {
-		data.addSubject(l.getObject());
+		data.addSubject(l.getObjectLabel());
 	    } else if ("http://purl.org/dc/terms/subject".equals(l
 		    .getPredicate())) {
-		data.addSubject(l.getObject());
+		data.addSubject(l.getObjectLabel());
+	    } else if ("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+		    .equals(l.getPredicate())) {
+		data.addType(l.getObject());
+		if ("http://purl.org/ontology/bibo/Periodical".equals(l
+			.getObject())) {
+		    data.addType("doc-type:Periodical");
+		} else if ("http://purl.org/ontology/bibo/Book".equals(l
+			.getObject())) {
+		    data.addType("doc-type:book");
+		}
 	    }
 	}
 	data.addIdentifier(node.getAggregationUri(), "hbz:edowebId");
