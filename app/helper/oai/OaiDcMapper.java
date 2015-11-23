a84e453e5f8292859f1d576936a264db9f77089e
Jan Schnasse <jan@schnasse.eu>
2015-11-23 09:55:43 +0100

    add doi and urn to oai_dc
    clean up mets output

--- app/helper/oai/OaiDcMapper.java
+++ app/helper/oai/OaiDcMapper.java
@@ -101,27 +101,13 @@ public class OaiDcMapper {
 	    } else if ("http://purl.org/dc/terms/issued".equals(l
 		    .getPredicate())) {
 		data.addDate(l.getObject());
-	    }
-	    // else if ("http://purl.org/dc/elements/1.1/contributor".equals(l
-	    // .getPredicate())) {
-	    // data.addContributor(l.getObject());
-	    // }
-	    // else if ("http://purl.org/dc/elements/1.1/creator".equals(l
-	    // .getPredicate())) {
-	    // data.addCreator(l.getObject());
-	    // }
-	    else if ("http://purl.org/dc/terms/contributor".equals(l
+	    } else if ("http://purl.org/dc/terms/contributor".equals(l
 		    .getPredicate())) {
 		data.addContributor(l.getObjectLabel());
 	    } else if ("http://purl.org/dc/terms/creator".equals(l
 		    .getPredicate())) {
 		data.addCreator(l.getObjectLabel());
-	    }
-	    // else if ("http://purl.org/dc/elements/1.1/publisher".equals(l
-	    // .getPredicate())) {
-	    // data.addPublisher(l.getObject());
-	    // }
-	    else if ("http://purl.org/lobid/lv#urn".equals(l.getPredicate())) {
+	    } else if ("http://purl.org/lobid/lv#urn".equals(l.getPredicate())) {
 		data.addIdentifier(l.getObject(), "hbz-urn");
 	    } else if ("http://purl.org/lobid/lv#hbzID"
 		    .equals(l.getPredicate())) {
@@ -129,13 +115,8 @@ public class OaiDcMapper {
 	    } else if ("http://purl.org/ontology/bibo/isbn".equals(l
 		    .getPredicate())) {
 		data.addIdentifier(l.getObject(), "hbz-isbn");
-	    }
-	    // else if ("http://purl.org/dc/elements/1.1/subject".equals(l
-	    // .getPredicate())) {
-	    // data.addSubject(l.getObject());
-	    // }
-	    else if ("http://purl.org/dc/terms/subject"
-		    .equals(l.getPredicate())) {
+	    } else if ("http://purl.org/dc/terms/subject".equals(l
+		    .getPredicate())) {
 		data.addSubject(l.getObjectLabel());
 	    } else if ("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 		    .equals(l.getPredicate())) {
@@ -156,6 +137,10 @@ public class OaiDcMapper {
 	    }
 	}
 	data.addIdentifier(node.getAggregationUri(), "hbz-edowebId");
+	if (node.hasUrn())
+	    data.addIdentifier(node.getUrn(), "hbz-urn");
+	if (node.hasDoi())
+	    data.addIdentifier(node.getDoi(), "hbz-doi");
 	data.addType("regal:" + node.getContentType());
 	return data;
     }
