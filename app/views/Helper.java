494b10769af09588d3baf3b9199a9f9b700e467e
Jan Schnasse <jan@schnasse.eu>
2017-07-31 13:17:42 +0200

    change link symbols to octicon link-external

--- app/views/Helper.java
+++ app/views/Helper.java
@@ -214,6 +214,7 @@ public class Helper {
 				subject.put("label", name);
 				subject.put("source", source);
 				subject.put("sourceId", sourceId);
+				subject.put("sourceName", getSubjectSource(sourceId, uri));
 				result1.add(subject);
 			}
 		}
