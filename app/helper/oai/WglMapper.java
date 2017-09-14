5e53601ad6cbed83cd32c495c978d11852db68d7
Jan Schnasse <schnasse@gmx.de>
2017-09-14 17:29:25 +0200

    license

--- app/helper/oai/WglMapper.java
+++ app/helper/oai/WglMapper.java
@@ -1,3 +1,20 @@
+/*Copyright (c) 2017 "hbz"
+
+This file is part of lobid-rdf-to-json.
+
+etikett is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package helper.oai;
 
 import java.util.ArrayList;
@@ -10,6 +27,10 @@ import models.DublinCoreData;
 import models.Globals;
 import models.Node;
 
+/**
+ * @author Jan Schnasse
+ *
+ */
 public class WglMapper {
 
 	OaiDcMapper dcMapper;
