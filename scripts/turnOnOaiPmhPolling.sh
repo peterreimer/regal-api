d31ae30ba9c3b55ce41024ea91af42ac63226e5a
Jan Schnasse <jan@schnasse.eu>
2018-01-26 11:09:59 +0100

    scripts are now in https://github.com/edoweb/regal-scripts

--- scripts/turnOnOaiPmhPolling.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $scriptdir
-source variables.conf
-
-mysql -uproai -p$REGAL_PASSWORD -e"UPDATE proai.rcAdmin SET pollingEnabled=1;"
-
-cd -
