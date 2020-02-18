9a0a5f020ed4e63f62b189eca330282adead5761
Andres Quast <quast@hbz-nrw.de>
2020-02-18 17:49:36 +0100

    solved issues with Calendar object

--- app/models/EmbargoModel.java
+++ app/models/EmbargoModel.java
@@ -11,28 +11,40 @@ import java.util.Calendar;
  */
 public class EmbargoModel {
 
+	/**
+	 * @param value
+	 * @return
+	 */
 	public Calendar setEmbargoDate(String value) {
 		String[] valueSplit = value.split("-");
 
 		Calendar embargoCal = Calendar.getInstance();
 		embargoCal.clear();
-		embargoCal.add(Calendar.YEAR, Integer.parseInt(valueSplit[0]));
-		embargoCal.add(Calendar.MONTH, Integer.parseInt(valueSplit[1]));
-		embargoCal.add(Calendar.DAY_OF_MONTH, Integer.parseInt(valueSplit[2]));
+		embargoCal.set(Calendar.YEAR, Integer.parseInt(valueSplit[0]));
+		embargoCal.set(Calendar.MONTH, Integer.parseInt(valueSplit[1]) - 1);
+		embargoCal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(valueSplit[2]));
 		return embargoCal;
 	}
 
+	/**
+	 * @param embargoTime
+	 * @return
+	 */
 	public boolean isActiveEmbargo(String embargoTime) {
 		Calendar cal = setEmbargoDate(embargoTime);
-		boolean test = cal.after(Calendar.getInstance().getTime());
+		boolean test = cal.getTime().after(Calendar.getInstance().getTime());
 
 		return test;
 	}
 
+	/**
+	 * @param embargoTime
+	 * @return
+	 */
 	public String getAccessScheme(String embargoTime) {
 		Calendar cal = setEmbargoDate(embargoTime);
 		String schemeValue = "public";
-		if (cal.after(Calendar.getInstance().getTime())) {
+		if (cal.getTime().after(Calendar.getInstance().getTime())) {
 			schemeValue = "embargoed";
 		}
 		;
