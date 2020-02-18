e523c1fd09ea06128501d08adde225277440c8a3
Andres Quast <quast@hbz-nrw.de>
2020-02-18 18:12:27 +0100

    fixed string in EmbargoModel

--- app/models/CoarModel.java
+++ app/models/CoarModel.java
@@ -8,25 +8,32 @@ import java.util.Hashtable;
 /**
  * @author aquast
  *
- * Class for representing COAR Values as constants
+ *         Class for representing COAR Values as constants
  */
 public abstract class CoarModel {
-	
+
 	public static final String OPEN_ACCESS = new String("open access");
-	public static final String OPEN_ACCESS_URI = new String("http://purl.org/coar/access_right/c_abf2");
+	public static final String OPEN_ACCESS_URI =
+			new String("http://purl.org/coar/access_right/c_abf2");
 	public static final String EMBARGOED_ACCESS = new String("embargoed access");
-	public static final String EMBARGOED_ACCESS_URI = new String("http://purl.org/coar/access_right/c_f1cf");
-	public static final String RESTRICTED_ACCESS = new String("restricted access");
-	public static final String RESTRICTED_ACCESS_URI = new String("http://purl.org/coar/access_right/c_16ec");
-	public static final String METADATA_ONLY_ACCESS = new String("metadata only access");
-	public static final String METADATA_ONLY_ACCESS_URI = new String("http://purl.org/coar/access_right/c_14cb");
+	public static final String EMBARGOED_ACCESS_URI =
+			new String("http://purl.org/coar/access_right/c_f1cf");
+	public static final String RESTRICTED_ACCESS =
+			new String("restricted access");
+	public static final String RESTRICTED_ACCESS_URI =
+			new String("http://purl.org/coar/access_right/c_16ec");
+	public static final String METADATA_ONLY_ACCESS =
+			new String("metadata only access");
+	public static final String METADATA_ONLY_ACCESS_URI =
+			new String("http://purl.org/coar/access_right/c_14cb");
 
 	public static Hashtable<String, String> elementContent = new Hashtable<>();
-	public static Hashtable<String, String> uriAttributeContent = new Hashtable<>();
-	
+	public static Hashtable<String, String> uriAttributeContent =
+			new Hashtable<>();
+
 	private static void setElementHashtable() {
 		elementContent.put("public", CoarModel.OPEN_ACCESS);
-		elementContent.put("embargo", CoarModel.EMBARGOED_ACCESS);
+		elementContent.put("embargoed", CoarModel.EMBARGOED_ACCESS);
 		elementContent.put("restricted", CoarModel.RESTRICTED_ACCESS);
 		elementContent.put("metadata", CoarModel.METADATA_ONLY_ACCESS);
 	}
@@ -37,9 +44,10 @@ public abstract class CoarModel {
 		uriAttributeContent.put("restricted", CoarModel.RESTRICTED_ACCESS_URI);
 		uriAttributeContent.put("metadata", CoarModel.METADATA_ONLY_ACCESS_URI);
 	}
-	
+
 	/**
 	 * mapping for lobid to coar
+	 * 
 	 * @param value
 	 * @return the element value according coar schema
 	 */
@@ -50,6 +58,7 @@ public abstract class CoarModel {
 
 	/**
 	 * mapping for lobid to coar
+	 * 
 	 * @param value
 	 * @return the uri attribute value according coar schema
 	 */
@@ -59,4 +68,3 @@ public abstract class CoarModel {
 	}
 
 }
-
